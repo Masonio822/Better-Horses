8ea65c253e5532b4bbfffaae83f4b02e82957e12
Masonio822 <masonelzy@gmail.com>
2025-01-13 15:19:35 -0500

    Initial Commit

---
+++ src/main/java/com/betterhorses/BetterHorsesClient.java
@@ -0,0 +1,10 @@
+package com.betterhorses;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterHorsesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
